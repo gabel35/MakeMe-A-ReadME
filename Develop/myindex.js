4eab54ffcb7e0241940c1c8bf269508cdb6395fd
gabel35 <gabrieljose3135@gmail.com>
2021-01-18 12:34:27 -0500

    my own version

--- Develop/myindex.js
+++ Develop/myindex.js
@@ -1,9 +1,8 @@
-// packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer')
 
-// array of questions for user input
-const questions = [
+inquirer
+    .prompt([
     {
         type: "input",
         message: "What is the title of your project?",
@@ -26,7 +25,12 @@ const questions = [
     },
     {
         type: "input",
-        message: "How can others contribute to this project?",
+        message: "Is there anyone you would like to give credit to, and why?",
+        name: "credits"
+    },
+    {
+        type: "input",
+        message: "How can others contribute to this project (guidelines)?",
         name: "contributing"
     },
     {
@@ -55,14 +59,80 @@ const questions = [
         message: "How should others contact you for questions?",
         name: "questions"
     },
-];
+    {
+        type: "input",
+        message: "Please provide the link to your project (an http/https url):",
+        name: "link"
+    },
+    {
+        type: "input",
+        message: "Please provide a pathway to an image or screenshot of your project (ex. ./Assets/image.PNG):",
+        name: "image"
+    }
+    ])
+    .then((data) => {
+        const {title, description, installation, usage, link, image, credits, contributing, tests, license, username, email, questions} = data;
+        const generateReadme = `
+        # ${title}
 
-// Description - description, 
-// Table of Contents - taken to the corresponding section of the README
-// Installation -installation instructions, 
-// Usage - usage information, 
-// Contributing - contribution guidelines, 
-// Tests - test instructions
+        ## Description
+
+        ${description}
+
+        ## Table of Contents
+
+        * [Installation](#installation)
+        * [Usage](#usage)
+        * [Credits](#credits)
+        * [Contributing](#contributing)
+        * [Tests](#tests)
+        * [License](#license)
+        * [Questions](#questions)
+
+
+        ## Installation
+
+        ${installation}
+
+
+        ## Usage
+
+        ${usage}
+
+        Please follow the following link to the application!
+        !(${link})
+
+        ![weather-app](${image})
+
+
+        ## Credits
+
+        ${credits}
+
+        ### Contributing
+
+        ${contributing}
+        
+        ### Tests
+
+        ${tests}
+        
+        ### License
+
+        ${license}
+
+        ### Questions
+
+        !(https://github.com/${username})
+        ${questions}
+
+        -------------
+
+        Thank you!
+
+        `
+    })
+
 // License - choose a license for my application from a list of options
 // THEN a badge for that license is added near the top of the README and a notice is added to the License section that explains which license the application is covered under
 // Questions - GitHub username with a link to my GitHub profile, email address with instructions on how to reach me with additional questions
@@ -70,7 +140,6 @@ const questions = [
 // TODO: Create a function to write README file
 
 function writeToFile(fileName, data) {
-    const {title, description, installation, usage, contributing, tests, license, username, email, questions} = data;
     const fileName = "README.md";
 
 }
